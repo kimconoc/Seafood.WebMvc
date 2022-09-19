098c862db1c250cd14710135b4f2598bbe9b167f
DESKTOP-J816ODH\nguye <kimconoc@gmail.com>
2022-09-19 22:22:17 +0700

    update

--- StoreProduct.WebMvc/Admin/assets/js/Constant.js
+++ StoreProduct.WebMvc/Admin/assets/js/Constant.js
@@ -40,7 +40,22 @@ function validatePassword(pw1,pw2)
     }
 
     return isPw;
+
 }
+
+function executeHourglass()
+{
+    var hourglass;
+    hourglass = document.getElementById("loadingHourglass");
+    hourglass.innerHTML = "&#xf251;";
+    setTimeout(function () {
+        hourglass.innerHTML = "&#xf252;";
+    }, 1000);
+    setTimeout(function () {
+        hourglass.innerHTML = "&#xf253;";
+    }, 2000);
+}
+
 /*Notification*/
 function HtmlNotifiError(content)
 {

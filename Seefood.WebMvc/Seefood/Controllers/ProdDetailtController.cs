a2123080a8d1eb4de37a117d37cac56b0e694d52
ducpv13 <ducpv13@vingroup.net>
2022-10-06 13:39:18 +0700

    Update

---
+++ Seefood.WebMvc/Seefood/Controllers/ProdDetailtController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Seefood.Controllers
+{
+    public class ProdDetailtController : BaseController
+    {
+        // GET: ProdDetailt
+        public ActionResult Detailt(string idProd)
+        {
+            return View();
+        }
+    }
+}

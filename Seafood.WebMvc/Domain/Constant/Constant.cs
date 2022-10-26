810a0d38c2f360897fefd0fdccab66e839bc1b7e
ducpv13 <ducpv13@vingroup.net>
2022-10-26 17:18:11 +0700

    push

--- Seafood.WebMvc/Domain/Constant/Constant.cs
+++ Seafood.WebMvc/Domain/Constant/Constant.cs
@@ -8,6 +8,8 @@ namespace Domain.Constant
 {
     public class Constant
     {
+        public const string Link_Img = "https://frontend.tikicdn.com/_desktop-next/static/img/account/empty-order.png";
+
         public const string CodeTom = "SEATOM";
         public const string CodeCua = "SEACUA";
         public const string CodeCa = "SEACA";

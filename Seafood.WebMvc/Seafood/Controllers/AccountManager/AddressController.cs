474fb8f4465a60e9cc6f977542beac130a693ec4
ducpv13 <ducpv13@vingroup.net>
2022-12-26 18:00:35 +0700

    update address

--- Seafood.WebMvc/Seafood/Controllers/AccountManager/AddressController.cs
+++ Seafood.WebMvc/Seafood/Controllers/AccountManager/AddressController.cs
@@ -61,10 +61,50 @@ namespace Seafood.Controllers.AccountManager
             }
             return Json(Success_Request(isCreate.Result.Data));
         }
-        public ActionResult AddressEdit()
+        public ActionResult AddressEdit(Guid addressId)
         {
+            if(addressId == null)
+            {
+                return RedirectToAction("AddressList", "Address");
+            }
+            var uri = ApiUri.Get_GetAddressByUserId + string.Format($"?addressId={addressId}");
+            var addresse = provider.GetAsync<Addresse>(uri);
+            if (addresse == null || addresse.Result == null || addresse.Result.Data == null)
+            {
+                return RedirectToAction("AddressList", "Address");
+            }
+            var result = addresse.Result.Data;
             ViewBag.IdItem = Constant.Id_So_Dia_Chi;
-            return View();
+            return View(result);
+        }
+        [HttpPost]
+        public ActionResult UpdateAddress(Guid Id,
+            string fullName, string mobile, string codeRegion, string codeDistrict, string codeWard, int typeAddress, bool isAddressMain, string address)
+        {
+            if (string.IsNullOrEmpty(mobile) || !Helper.ValidPhoneNumer(mobile))
+            {
+                return Json(Bad_Request("Số điện thoại không hợp lệ"));
+            }
+            var userData = GetCurrentUser();
+            dynamic model = new
+            {
+                Id = Id,
+                UserId = userData.UserId,
+                FullName = fullName,
+                Mobile = mobile,
+                CodeRegion = codeRegion,
+                CodeDistrict = codeDistrict,
+                CodeWard = codeWard,
+                TypeAddress = typeAddress,
+                IsAddressMain = isAddressMain,
+                Address = address,
+            };
+            var isUpd = provider.PostAsync<bool>(ApiUri.Post_UpdateAddressByUserId, model);
+            if (isUpd == null || isUpd.Result == null || !isUpd.Result.Success)
+            {
+                return View(Server_Error());
+            }
+            return Json(Success_Request(isUpd.Result.Data));
         }
     }
 }

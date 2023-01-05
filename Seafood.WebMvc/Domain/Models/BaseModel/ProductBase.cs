831b6c5063232d3af00ea5427958471a69338127
ducpv13 <ducpv13@vingroup.net>
2023-01-05 12:03:38 +0700

    update

--- Seafood.WebMvc/Domain/Models/BaseModel/ProductBase.cs
+++ Seafood.WebMvc/Domain/Models/BaseModel/ProductBase.cs
@@ -11,8 +11,6 @@ namespace Domain.Models.BaseModel
     public class ProductBase : VBaseModel
     {
         public string CategoryCode { get; set; }
-        public string RegionDistrictCode { get; set; }
-        public string RegionCode { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public double? ReviewProd { get; set; }

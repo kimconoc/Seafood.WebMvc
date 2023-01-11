cedb848139c1072fa691e25f29e04cca16408ebc
ducpv13 <ducpv13@vingroup.net>
2023-01-11 14:23:41 +0700

    push

--- Seafood.WebMvc/Domain/Models/ResponseModel/User.cs
+++ Seafood.WebMvc/Domain/Models/ResponseModel/User.cs
@@ -10,7 +10,7 @@ namespace Domain.Models.ResponseModel
         public string DisplayName { get; set; }
         public string Avarta { get; set; }
         public DateTime Birthday { get; set; }
-        public int Sex { get; set; }
+        public int? Sex { get; set; }
         public string Mobile { get; set; }
         public string Email { get; set; }
     }
--- Seafood.WebMvc/Seafood/Models/UserData.cs
+++ Seafood.WebMvc/Seafood/Models/UserData.cs
@@ -12,7 +12,7 @@ namespace Seafood.Models
         public string DisplayName { get; set; }
         public string Avarta { get; set; }
         public DateTime Birthday { get; set; }
-        public int Sex { get; set; }
+        public int? Sex { get; set; }
         public string Mobile { get; set; }
         public string Email { get; set; }
     }
